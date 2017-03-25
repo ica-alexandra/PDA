200c69ef3ba07b269db3744b762c4027b33e0e40
ica-alexandra <alexandra_claudiaica@yahoo.com>
2017-03-25 22:53:21 +0200

    tema 2 (MPI) prob 2

---
+++ sumMPI/sumMPI/Source.cpp
@@ -0,0 +1,62 @@
+#include "mpi.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define SIZE 100
+
+int* generate_rand_nums(int rank) {
+	int i;
+	int arr[SIZE];
+
+	srand(time(NULL) + rank);
+
+	for (i = 0; i < SIZE; i++) {
+		arr[i] = rand() % 1000;
+		printf("%d, ", arr[i]);
+	}
+
+	return arr;
+}
+
+int compute_sum(int* arr) {
+	int i, s = 0;
+	for (i = 0; i < SIZE; i++)
+		s += arr[i];
+	return s;
+}
+int main(int argc, char *argv[])
+{
+	int rank, size;
+	int* procArr;
+	double start_time[10], end_time[10];
+
+	for (int i = 0; i < 6; i++) {
+		start_time[i] = 0;
+		end_time[i] = 0;
+	}
+
+	MPI_Init(&argc, &argv);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+	if (rank == 0) {
+		printf("Root process\n\n");
+	}
+		
+	if (rank != 0) {
+		start_time[rank] = MPI_Wtime();
+		printf("\nStart time for process %d: %d \n", rank, start_time[rank]);
+		printf("\nProcess %d elements: ", rank);
+		procArr = generate_rand_nums(rank);
+		int sum = compute_sum(procArr);
+		printf("\nSum is: %d ", sum);
+		end_time[rank] = MPI_Wtime();
+		printf("\nFinish time for process %d: %d \n", rank, end_time[rank]);	
+		printf("\nTime elapsed for process %d: %d\n", rank, (end_time[rank] - start_time[rank]));
+	}
+	
+	MPI_Finalize();
+
+}
+
